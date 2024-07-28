a6f22561a4c0b1c7b0534b24b56aa9661f2f2b30
doyled-it <michaeldoyle1994@gmail.com>
2024-07-28 19:25:43 +0000

    Deploying to gh-pages from @ doyled-it/theneedlepoint@05c23c26387205b6f85dd34e2c3f77ba5418636b 🚀

--- src/react-plotly.d.ts
+++
@@ -1,7 +0,0 @@
-declare module "react-plotly.js" {
-  import { Component } from "react";
-  import { PlotParams } from "plotly.js";
-
-  class Plot extends Component<Partial<PlotParams>> {}
-  export default Plot;
-}
--- src/utils.ts
+++
@@ -1,4 +0,0 @@
-export const safeFloat = (value: any): number => {
-  const parsed = parseFloat(value);
-  return isNaN(parsed) ? 0 : parsed; // Return 0 instead of null
-};
